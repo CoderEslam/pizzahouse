4571e0883a4a8f81486d83d8b0c81e01676bd1f8
eslam <eslamfgazy600@gmail.com>
2022-12-08 01:56:03 +0200

    learn css - styles separated

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -1,52 +1,13 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div style="background-color: #a7c7ff">
-        <div style="color: darkblue ;text-align: center">
+    <div style="height: 100vh;">
+        <div class="title">
             <h1>
                 Pizza House <br/>
                 The North's Best
             </h1>
-            {{--        <p>--}}
-            {{--        {{$type}} - {{$base}} - {{$price}}--}}
-            {{--        </p>--}}
-            @for($i = 0;$i < count($pizzas);$i++)
-                <p> {{$pizzas[$i]['type']}} - {{$pizzas[$i]['base']}} - {{$pizzas[$i]['price']}}</p>
-            @endfor
-
-            @foreach($pizzas as $pizza)
-                <div style="color: chartreuse">
-                    {{$loop->index }}  {{$pizza['type']}} - {{$pizza['base']}} - {{$pizza['price']}}
-                    @if($loop->first)
-                        <span>- first in the loop</span>
-                    @endif
-                    @if($loop->last)
-                        <span>- last in the loop</span>
-                    @endif
-                </div>
-            @endforeach
-
         </div>
     </div>
 @endsection
 
-@section('forloop')
-    <div style="background-color: #fff1d8">
-        <div style="color: darkblue ;text-align: center">
-
-            <h1>for each Loop</h1>
-            <ul>
-                @php
-                    $articles = ['Article 1','Article 2','Article 3'];
-                @endphp
-                @foreach ($articles as $article)
-                    <li>{{ $article }}</li>
-                @endforeach
-                @foreach ($articles as $article)
-                    <li>{{ $article }}</li>
-                @endforeach
-            </ul>
-
-        </div>
-    </div>
-@endsection
---
+++ resources/views/pizzasTest.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.layout')
+
+@section('content')
+    <div style="background-color: #a7c7ff">
+        <div style="text-align: center ;background-color: #5e2195">
+            <h1>
+                Pizza House <br/>
+                The North's Best
+            </h1>
+            {{--        <p>--}}
+            {{--        {{$type}} - {{$base}} - {{$price}}--}}
+            {{--        </p>--}}
+            @for($i = 0;$i < count($pizzas);$i++)
+                <p> {{$pizzas[$i]['type']}} - {{$pizzas[$i]['base']}} - {{$pizzas[$i]['price']}}</p>
+            @endfor
+
+            @foreach($pizzas as $pizza)
+                <div style="color: chartreuse">
+                    {{$loop->index }}  {{$pizza['type']}} - {{$pizza['base']}} - {{$pizza['price']}}
+                    @if($loop->first)
+                        <span>- first in the loop</span>
+                    @endif
+                    @if($loop->last)
+                        <span>- last in the loop</span>
+                    @endif
+                </div>
+            @endforeach
+
+        </div>
+    </div>
+@endsection
+
+@section('forloop')
+    <div style="background-color: #fff1d8">
+        <div style="color: darkblue ;text-align: center">
+            <img src="img/night.png" style="width: 100px ; height: 100px" alt="image">
+            <h1>for each Loop</h1>
+            <ul>
+                @php
+                    $articles = ['Article 1','Article 2','Article 3'];
+                @endphp
+                @foreach ($articles as $article)
+                    <li>{{ $article }}</li>
+                @endforeach
+                @foreach ($articles as $article)
+                    <li>{{ $article }}</li>
+                @endforeach
+            </ul>
+
+        </div>
+    </div>
+@endsection
