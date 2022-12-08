04b4ce85caad1fd5e7d4e7c13bb3260a2ce29c0a
eslam <eslamfgazy600@gmail.com>
2022-12-08 22:27:57 +0200

    learn connect with mysql database

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+/*
+ * to create a database
+ * create database pizzahouse;
+ */
+
 class PizzaController extends Controller
 {
     //
