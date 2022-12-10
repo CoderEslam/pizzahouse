faa88aea70518a2d26ed7b33129f2ba86380a4b7
eslam <eslamfgazy600@gmail.com>
2022-12-10 19:10:15 +0200

    learn show all order in line

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -3,21 +3,16 @@
 @section('content')
     <div style="height: 100vh;">
         <div class="title">
-            <p>{{$name}} - {{$age}}</p>
-
-            @for($i = 0;$i < count($pizzas);$i++)
-                <p> {{$pizzas[$i]['type']}} - {{$pizzas[$i]['base']}} - {{$pizzas[$i]['price']}}</p>
-            @endfor
-
-            <img src="img/night.png" style="width: auto;height: 200px">
             <h1>
                 Pizza House <br/>
                 The North's Best
             </h1>
-            <p>
-                {{session('message')}} {{-- to resive data from redirect() method in PizzaController--}}
-            </p>
-            <a href="/pizzas/create">Order a pizza</a>
+            @for($i = 0;$i < count($pizzas);$i++)
+                <div>
+                    <img src="img/pizza.jpg" style="height: 100px ; width: 100px">
+                    <p> <h4><a href="pizzas/show/{{$pizzas[$i]['id']}}}">{{$pizzas[$i]['name']}}</a></h4> </p>
+                </div>
+            @endfor
         </div>
     </div>
 @endsection
